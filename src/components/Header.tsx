dfe4e7eefa6a663739e5e7ca5eaf3b9e4565cde2
mitchan <hs0127sh@gmail.com>
2022-05-07 12:44:21 +0900

    🚑 🚑 🚑

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
     <header className="border-b border-gray-300 py-8">
       <Link href="/">
         <a>
-          <h1 className="text-5xl font-bold">みっつあんのブログん</h1>
+          <h1 className="text-5xl font-bold">みっつつあんのブログん</h1>
         </a>
       </Link>
     </header>
